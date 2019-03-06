a609d11edcaf1a9e9bdae20f84fb9baccebf8a68
Marcelo Machado <marcelo.cruz.machado@hotmail.com>
2019-03-06 00:39:10 -0300

    Classe de teste
    Classe de teste de geração de senha.

---
+++ test/testeSenha.java
@@ -0,0 +1,20 @@
+
+import ClassesAuxiliares.CodificarSenha;
+import java.security.NoSuchAlgorithmException;
+
+public class testeSenha {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        //TESTE DE COMO USAR A SENHA
+        CodificarSenha codificarSenha = new CodificarSenha();
+        String senhaInformada = "testeSenha";
+
+        String saltComSHA256 = codificarSenha.gerarHashSHA256(codificarSenha.gerarSalt(senhaInformada.length()));
+        String senhaFinalSHA256 = codificarSenha.criptografarSenha(senhaInformada, saltComSHA256);
+
+        System.out.println("Senha = " + senhaFinalSHA256);
+        System.out.println("Salt = " + saltComSHA256);
+    }
+
+}
